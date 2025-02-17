66bc7a4e738b34e779bd0fe0a02a3284956a57cc
unknown <abermehdi@yahoo.fr>
2025-02-17 20:59:52 +0100

    ok

--- braqueo-landing/src/app/page1.tsx
+++ braqueo-landing/src/app/page1.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto text-center">
             <h1 className="text-4xl md:text-5xl font-bold mb-6">
-              Vous y avez cru ?
+              Vous y avez cru ssss?
             </h1>
             <p className="text-xl mb-8">
               
